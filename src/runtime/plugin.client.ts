08bb10fabc1fc56d4fea44e78bebd34e22f2b09a
いーちゃん(e_chan1007) <echan041007@gmail.com>
2022-08-23 17:27:40 +0900

    chore: v1.0.0-alpha.3

--- src/runtime/plugin.client.ts
+++ src/runtime/plugin.client.ts
@@ -1,3 +1,4 @@
+import { defineNuxtPlugin } from '#app'
 import type * as Monaco from 'monaco-editor'
 import monacoLoader from 'monaco-editor/min/vs/loader?url'
 import { _useMonacoState } from './composables'
