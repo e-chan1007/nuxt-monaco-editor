04f4bbb4fbee89940108f2c54c03561f065ff9ad
いーちゃん(e_chan1007) <echan041007@gmail.com>
2022-10-21 22:28:05 +0900

    fix: follow changes in v3.0.0-rc.12

--- src/module.ts
+++ src/module.ts
@@ -1,5 +1,5 @@
 import { fileURLToPath } from 'url'
-import { defineNuxtModule, addComponent, addAutoImport, addPlugin, createResolver, addVitePlugin } from '@nuxt/kit'
+import { defineNuxtModule, addComponent, addPlugin, createResolver, addImports, extendViteConfig, checkNuxtCompatibility } from '@nuxt/kit'
 import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export type MonacoEditorLocale = 'de' | 'es' | 'fr' | 'it' | 'ja' | 'ko' | 'ru' | 'zh-cn' | 'zh-tw' | 'en';
@@ -26,7 +26,7 @@ export default defineNuxtModule<ModuleOptions>({
   meta: {
     name: 'nuxt-monaco-editor',
     configKey: 'monacoEditor',
-    compatibility: { nuxt: '^3.0.0-rc.8' }
+    compatibility: { nuxt: '^3.0.0-rc.9' }
   },
   defaults: DEFAULTS,
   setup (options, nuxt) {
@@ -38,21 +38,24 @@ export default defineNuxtModule<ModuleOptions>({
     nuxt.options.build.transpile.push('monaco-editor')
     nuxt.options.runtimeConfig.app.__MONACO_EDITOR_LOCALE__ = options.locale
     nuxt.options.runtimeConfig.app.__MONACO_EDITOR_LOCATION__ = monacoEditorLocation
-    const [viteStaticCopyServePlugin, viteStaticCopyBuildPlugin] = viteStaticCopy({
-      targets: [{
-        src: require
-          .resolve('monaco-editor/min/vs/loader.js')
-          .replace(/\\/g, '/')
-          .replace(/\/vs\/loader\.js$/, '/*'),
-        dest: monacoEditorLocation.slice(1)
-      }]
+
+    extendViteConfig(async (config) => {
+      const shouldAppendURLPrefix = await checkNuxtCompatibility({ nuxt: '^3.0.0-rc.12' }) && nuxt.options.dev
+      const viteStaticCopyPlugin = viteStaticCopy({
+        targets: [{
+          src: require
+            .resolve('monaco-editor/min/vs/loader.js')
+            .replace(/\\/g, '/')
+            .replace(/\/vs\/loader\.js$/, '/*'),
+          dest: (shouldAppendURLPrefix ? '__url/' : '') + monacoEditorLocation.slice(1)
+        }]
+      })
+      config.plugins.push(...viteStaticCopyPlugin)
     })
-    addVitePlugin(viteStaticCopyServePlugin)
-    addVitePlugin(viteStaticCopyBuildPlugin)
 
     addPlugin(resolve('plugin.client'))
     addComponent({ name: options.componentName!.codeEditor!, filePath: resolve('MonacoEditor.vue') })
     addComponent({ name: options.componentName!.diffEditor!, filePath: resolve('MonacoDiffEditor.vue') })
-    addAutoImport({ name: 'useMonaco', as: 'useMonaco', from: resolve('composables') })
+    addImports({ name: 'useMonaco', as: 'useMonaco', from: resolve('composables') })
   }
 })
