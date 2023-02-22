7f88db5ea359ae0b91ea7664872b9f908f26994a
いーちゃん(e_chan1007) <echan041007@gmail.com>
2023-02-23 00:04:22 +0900

    fix: change the directory to which `monaco-editor` will be copied

--- src/module.ts
+++ src/module.ts
@@ -1,5 +1,5 @@
 import { fileURLToPath } from 'url'
-import { defineNuxtModule, addComponent, addPlugin, createResolver, addImports, extendViteConfig, checkNuxtCompatibility } from '@nuxt/kit'
+import { defineNuxtModule, addComponent, addPlugin, createResolver, addImports, addVitePlugin } from '@nuxt/kit'
 import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export type MonacoEditorLocale = 'de' | 'es' | 'fr' | 'it' | 'ja' | 'ko' | 'ru' | 'zh-cn' | 'zh-tw' | 'en';
@@ -30,28 +30,27 @@ export default defineNuxtModule<ModuleOptions>({
   },
   defaults: DEFAULTS,
   setup (options, nuxt) {
+    const isDev = nuxt.options.dev
+    options.dest = (isDev ? '_nuxt_monaco_editor/' : '_nuxt/') + options.dest
     const runtimeDir = fileURLToPath(new URL('./runtime', import.meta.url))
     const { resolve: resolveURL } = createResolver(nuxt.options.app.baseURL)
     const { resolve } = createResolver(runtimeDir)
-    const monacoEditorLocation = resolveURL(options.dest!)
+    const monacoEditorLocation = resolveURL(options.dest)
     nuxt.options.build.transpile.push(runtimeDir)
     nuxt.options.build.transpile.push(({ isClient }) => isClient ? 'monaco-editor' : false)
     nuxt.options.runtimeConfig.app.__MONACO_EDITOR_LOCALE__ = options.locale!
     nuxt.options.runtimeConfig.app.__MONACO_EDITOR_LOCATION__ = monacoEditorLocation
 
-    extendViteConfig((config) => {
-      const viteStaticCopyPlugin = viteStaticCopy({
-        targets: [{
-          src: require
-            .resolve('monaco-editor/min/vs/loader.js')
-            .replace(/\\/g, '/')
-            .replace(/\/vs\/loader\.js$/, '/*'),
-          dest: options.dest!
-        }]
-      })
-      if (!config.plugins) { config.plugins = [] }
-      config.plugins.push(...viteStaticCopyPlugin)
+    const viteStaticCopyPlugin = viteStaticCopy({
+      targets: [{
+        src: require
+          .resolve('monaco-editor/min/vs/loader.js')
+          .replace(/\\/g, '/')
+          .replace(/\/vs\/loader\.js$/, '/*'),
+        dest: options.dest
+      }]
     })
+    addVitePlugin(viteStaticCopyPlugin)
 
     addPlugin(resolve('plugin.client'))
     addComponent({ name: options.componentName!.codeEditor!, filePath: resolve('MonacoEditor.vue') })
