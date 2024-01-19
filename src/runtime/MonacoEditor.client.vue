d1b0783f1332fd14a1ea6d4498b1e369433008b6
Larry Williamson <l422y@l422y.com>
2024-01-19 16:16:25 -0500

    chore: lint

--- src/runtime/MonacoEditor.client.vue
+++ src/runtime/MonacoEditor.client.vue
@@ -5,9 +5,9 @@
 </template>
 
 <script lang="ts" setup>
-import type * as Monaco from "monaco-editor"
-import { computed, ref, shallowRef, watch, onBeforeUnmount } from "vue"
-import { defu } from "defu"
+import type * as Monaco from 'monaco-editor'
+import { computed, ref, shallowRef, watch, onBeforeUnmount } from 'vue'
+import { defu } from 'defu'
 import { useMonaco } from './composables'
 
 interface Props {
@@ -24,15 +24,14 @@ interface Props {
 }
 
 interface Emits {
-    (event: "update:modelValue", value: string): void
-
-    (event: "load", editor: Monaco.editor.IStandaloneCodeEditor): void
+    (event: 'update:modelValue', value: string): void
+    (event: 'load', editor: Monaco.editor.IStandaloneCodeEditor): void
 }
 
 const props = withDefaults(defineProps<Props>(), {
-    lang: () => "plaintext",
-    options: () => ( {} ),
-    modelValue: () => ""
+  lang: () => 'plaintext',
+  options: () => ({}),
+  modelValue: () => ''
 })
 
 const emit = defineEmits<Emits>()
@@ -42,53 +41,53 @@ const editorRef = shallowRef<Monaco.editor.IStandaloneCodeEditor>()
 const editorElement = ref<HTMLDivElement>()
 const monaco = useMonaco()!
 const defaultOptions: Monaco.editor.IStandaloneEditorConstructionOptions = {
-    automaticLayout: true
+  automaticLayout: true
 }
 
 let editor: Monaco.editor.IStandaloneCodeEditor
 let model: Monaco.editor.ITextModel
 
 watch(() => props.modelValue, () => {
-    if (editor?.getValue() !== props.modelValue) {
-        editor?.setValue(props.modelValue)
-    }
+  if (editor?.getValue() !== props.modelValue) {
+    editor?.setValue(props.modelValue)
+  }
 })
 
 watch(() => props.lang, () => {
-    if (model) {
-        model.dispose()
-    }
-    model = monaco.editor.createModel(props.modelValue, lang.value)
-    editor?.setModel(model)
+  if (model) {
+    model.dispose()
+  }
+  model = monaco.editor.createModel(props.modelValue, lang.value)
+  editor?.setModel(model)
 })
 
 watch(() => props.options, () => {
-    editor?.updateOptions(defu(props.options, defaultOptions))
+  editor?.updateOptions(defu(props.options, defaultOptions))
 })
 
 watch(editorElement, (newValue, oldValue) => {
-    if (!editorElement.value || oldValue) return
-    editor = monaco.editor.create(editorElement.value!, defu(props.options, defaultOptions))
-    model = monaco.editor.createModel(props.modelValue, lang.value)
-    editorRef.value = editor
-    editor.layout()
-    editor.setModel(model)
-    editor.onDidChangeModelContent(() => {
-        emit("update:modelValue", editor.getValue())
-    })
-    isLoading.value = false
-    emit("load", editor)
+  if (!editorElement.value || oldValue) { return }
+  editor = monaco.editor.create(editorElement.value!, defu(props.options, defaultOptions))
+  model = monaco.editor.createModel(props.modelValue, lang.value)
+  editorRef.value = editor
+  editor.layout()
+  editor.setModel(model)
+  editor.onDidChangeModelContent(() => {
+    emit('update:modelValue', editor.getValue())
+  })
+  isLoading.value = false
+  emit('load', editor)
 })
 
 defineExpose({
-    /**
+  /**
      * Monaco editor instance
      */
-    $editor: editorRef
+  $editor: editorRef
 })
 
 onBeforeUnmount(() => {
-    editor?.dispose()
-    model?.dispose()
+  editor?.dispose()
+  model?.dispose()
 })
 </script>
