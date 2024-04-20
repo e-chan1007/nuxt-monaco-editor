7d2a3793f8e31dc70aa4f3e829948be3e8dc69ec
いーちゃん(e_chan1007) <echan041007@gmail.com>
2024-04-20 19:36:54 +0900

    chore: add pasting workaround

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -6,5 +6,6 @@ export default defineNuxtConfig({
     MonacoEditorModule
   ],
   monacoEditor: {
+
   }
 })
--- src/vitePlugin.ts
+++ src/vitePlugin.ts
@@ -29,8 +29,11 @@ const plugin = (options: ModuleOptions, nuxtOptions: NuxtOptions): Plugin => ({
     }
   },
   async load (id) {
+    id = id.split('?')[0]
+
+    const vsPath = id.includes('monaco-editor/esm') ? id.split('monaco-editor/esm/').pop() : null
+
     if (options.locale !== 'en') {
-      id = id.split('?')[0]
       if (rewrittenMonacoFiles.has(id)) {
         return { code: rewrittenMonacoFiles.get(id)! }
       }
@@ -38,19 +41,33 @@ const plugin = (options: ModuleOptions, nuxtOptions: NuxtOptions): Plugin => ({
         const code = (await fs.readFile(resolve('nls.mjs'), 'utf-8'))
           .replace('__LOCALE_DATA_PATH__', `nuxt-monaco-editor/dist/i18n/${options.locale}.json`)
         rewrittenMonacoFiles.set(id, code)
+
         return { code }
       }
-      if (/monaco-editor\/esm\/vs.+\.js/.test(id)) {
-        const vsPath = id.split(/monaco-editor\/esm\//).pop()
-        if (vsPath) {
-          const path = vsPath.replace('.js', '')
-          const code = (await fs.readFile(id, 'utf-8'))
-            .replace(/import \* as nls from '.+nls\.js(\?v=.+)?';/g, `import * as nls from '${nlsPath}';`)
-            .replace(/(?<!function )localize\(/g, `localize('${path}', `)
-          rewrittenMonacoFiles.set(id, code)
-          return { code }
+
+      if (vsPath && id.endsWith('.js')) {
+        const path = vsPath.replace(/\.js$/, '')
+        let code = (await fs.readFile(id, 'utf-8'))
+          .replace(/import \* as nls from '.+nls\.js(\?v=.+)?';/g, `import * as nls from '${nlsPath}';`)
+          .replace(/(?<!function )localize\(/g, `localize('${path}', `)
+        if (path === 'vs/base/common/platform') {
+          // Workaround for not being able to paste in Monaco Editor
+          code = code.replace('let _isWeb = false;', 'let _isWeb = true;')
         }
+        rewrittenMonacoFiles.set(id, code)
+
+        return { code }
       }
+      return
+    }
+
+    if (vsPath === 'vs/base/common/platform.js') {
+      // Workaround for not being able to paste in Monaco Editor
+      let code = (await fs.readFile(id, 'utf-8'))
+      code = code.replace('let _isWeb = false;', 'let _isWeb = true;')
+      rewrittenMonacoFiles.set(id, code)
+
+      return { code }
     }
   }
 })
